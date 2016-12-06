b72183f4c79cfee264411515584b153a69707a71
billy3321 <billy3321@gmail.com>
2016-12-06 11:43:14 +0800

    fix content of sign.sh

--- sign.sh
+++ sign.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+# uncomment this for debug
+# set -o xtrace
+
 # This is a file use to sign. Please input file as parameter.
 # Usage: ./sign.sh signed_by_${USERNAME}.txt
 
 pushd $(dirname $0)
+REPO=$(cat .git/config | grep "git@github.com" | cut -f 2 -d ':' | sed s/\.git//)
 
 git pull
+git remote add upstream https://github.com/RainbowEngineer/taiwan_love_wins.git
+git fetch upstream
+git merge upstream/master
+git push
 git add $1
 git commit
 git push
-REPO=$(cat .git/config | grep "git@github.com" | cut -f 2 -d ':' | sed s/\.git//)
 
 if [ -f $(which hub) ]; then
   echo '自動送 pull-request...'
   hub pull-request -m "Please add my sign!!" -b RainbowEngineer/taiwan_love_wins:master
 else
-  
   echo '系統沒有安裝hub，請前往以下網址送出pull request：'
   echo "https://github.com/${REPO}/pull/new/master"
 fi
